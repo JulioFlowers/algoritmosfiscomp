4ac07549ed9c8cbffe3dc07a1010a8ecf0e8227a
Julio <julio_perez@ciencias.unam.mx>
2023-10-25 23:54:10 -0600

    puro julia y no mamadas

--- disparo.jl
+++ disparo.jl
@@ -1,23 +1,44 @@
+#=
+Perez Flores Julio A.
+Taller:
+
+Aplique la tecnica de disparo lineal con N=10 al problema de valor en la frontera
+
+y''= -(2/x)y'+(2(x²)y+(sin(ln x)/x²)
+=#
+
 using Plots
-n = 100
+
+#limites
+n = 10
 a = 1
 b = 2
 h = (b - a) / n
 x = a:h:b
+
+#puntos
 y = zeros(length(x))
 yr = zeros(length(x))
 dy = zeros(1, n)
+
+#condiciones
 y[1] = 1
 y[n] = 2
 dy[1] = 0
+
+#para la EDO
 p(x) = -2 / x
 q(x) = 2 / (x^2)
 r(x) = sin(log(x)) / (x^2)
+
+#f del problema
 fr(x) = 1.1392070132x - 0.03920701320 / (x^2) - 3 / 10 * sin(log(x)) - 1 / 10 * cos(log(x))
+
 u1 = zeros(1, n)
 u2 = zeros(1, n)
 v1 = zeros(1, n)
 v2 = zeros(1, n)
+
 u1[1] = 1
 u2[1] = 0
 v1[1] = 0
@@ -32,6 +53,7 @@ for i in 1:n-1
     k32 = h * (p(x[i] + h / 2) * (u2[i] + k22 / 2) + q(x[i] + h / 2) * (u1[i] + k21 / 2) + r(x[i] + h / 2))
     k41 = h * (u2[i] + k32)
     k42 = h * (p(x[i + 1]) * (u2[i] + k32) + q(x[i + 1]) * (u1[i] + k31) + r(x[i + 1]))
+
     u1[i + 1] = u1[i] + (k11 + 2 * k21 + 2 * k31 + k41) / 6
     u2[i + 1] = u2[i] + (k12 + 2 * k22 + 2 * k32 + k42) / 6
     
@@ -43,6 +65,7 @@ for i in 1:n-1
     dk32 = h * (p(x[i] + h / 2) * (v2[i] + dk22 / 2) + q(x[i] + h / 2) * (v1[i] + dk21 / 2))
     dk41 = h * (v2[i] + dk32)
     dk42 = h * (p(x[i + 1]) * (v2[i] + dk32) + q(x[i + 1]) * (v1[i] + dk31))
+
     v1[i + 1] = v1[i] + (dk11 + 2 * dk21 + 2 * dk31 + dk41) / 6
     v2[i + 1] = v2[i] + (dk12 + 2 * dk22 + 2 * dk32 + dk42) / 6
 end

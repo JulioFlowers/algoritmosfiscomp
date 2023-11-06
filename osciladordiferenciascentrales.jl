d682002d574abb8d74952647adb11c4a19c87264
Julio <julio_perez@ciencias.unam.mx>
2023-11-05 21:28:39 -0600

    yasalioelperreosciladorenjuliapinchelenguajehermoso

--- osciladordiferenciascentrales.jl
+++ osciladordiferenciascentrales.jl
@@ -1,8 +1,8 @@
 using Plots
 using LinearAlgebra
 
-function al(xi, h, n)::Float64
-    return (12*(ep-(xi+n*h)^2)*(h^2))-30
+function al(yi, h, n)::Float64
+    return (12*(ep-(yi+n*h)^2)*(h^2))-30
 end;
 
 function gauss_seidel(A, b, x0, tol, max_iter)
@@ -31,43 +31,57 @@ function gauss_seidel(A, b, x0, tol, max_iter)
     end
     
     println("El método de Gauss-Seidel no convergió en $max_iter iteraciones.")
-    return x
+    return 
 end
 
 
 function matrixBuilder()
 
-    M=Matrix{Int}(undef,0,n-1)
+    M=Matrix{Int}(undef,0,n-3)
+    
 
     #caso especial i=2
-    alp = al(xf[1], h, 2) 
+    alp = al(y1, h, 2) 
 
-    iccoef= Float64[16.0, alp, 16.0, -1.0]
-    aux= zeros(Float64,(n-5))
+    iccoef= Float64[alp, 16.0, -1.0]
+    aux= zeros(Float64,(n-6))
     icc = vec(vcat(iccoef,aux))'
     M=vcat(M,icc)
-    
-    
-    for i in 3:(n-3)
-        local alp = al(xf[1], h, i) 
-        gcoef = [-1, 16, alp, 16, 1]
-        aux= vcat(zeros(Float64,(i-3)), gcoef)
-        local icc = vec(vcat(aux, zeros(Float64,((n-1)-(i+2)))))'
-        M=vcat(M,icc)
-    end
-    
-    #caso especial i=n-2
-    alp = al(xf[1], h, (n-2)) 
-    fccoef=[-1, 16, alp, 16]
-    icc = vec(vcat(zeros(Float64,(n-5)), fccoef))'
+
+    #caso especial i=3
+    alp = al(y1, h, 3) 
+
+    iccoef= Float64[16.0, alp, 16.0, -1.0]
+    aux= zeros(Float64,(n-7))
+    icc = vec(vcat(iccoef,aux))'
     M=vcat(M,icc)
 
-    #rellena para hacer cuadrada
-    for i in 1:2
-        M=vcat(M,vec(zeros(Float64, n-1))')
+    
+    if (n-3 != 4)
+    
+        for i in 4:(n-4)
+            local alp = al(y1, h, i) 
+            gcoef = [-1.0, 16.0, alp, 16.0, -1.0]
+            aux= vcat(zeros(Float64,(i-4)), gcoef)
+            local icc = vec(vcat(aux, zeros(Float64,((n-3)-(i+1)))))'
+            println(icc)
+            M=vcat(M,icc)
+        end
+
     end
 
-    println(M)
+    #caso especial i=n-3
+    alp = al(y1, h, (n-3)) 
+    fccoef=[-1.0, 16.0, alp, 16.0]
+    icc = vec(vcat(zeros(Float64,(n-7)), fccoef))'
+    M=vcat(M,icc)
+    
+    #caso especial i=n-2
+    alp = al(y1, h, (n-2)) 
+    fccoef=[-1, 16, alp]
+    icc = vec(vcat(zeros(Float64,(n-6)), fccoef))'
+    M=vcat(M,icc)
+
     return M
 end
 
@@ -76,31 +90,33 @@ function rvecBuilder()
     vec=[]
 
     #caso especial i=2
-    push!(vec,(uxf[1]))
+    push!(vec,(uyf[1]-16*uyf[2]))
 
-    vec = vcat(vec,zeros(Float64,(n-5)))
+    #caso especial i=3
+    push!(vec,uyf[2])
+
+    vec = vcat(vec,zeros(Float64,(n-7)))
+
+    #caso especial i=3
+    push!(vec,uyf[3])
 
     #caso especial i=n-2
-    push!(vec,(uxf[2]))
-
-    #rellena para hacerla solucion de matrix cuadrada
-    vec = vcat(vec,[0.0,0.0])
+    push!(vec,(uyf[4]-16*uyf[3]))
 
     return vec
 end 
 
-println("Ingrese el primer  y el ultimo termino del intervalo, separados por una coma (x0, xn)")
-user_xf = readline()
-xf = split(user_xf, ',')
-xf= parse.(Float64, xf)
-println(xf)
+println("Ingrese el primer  y el ultimo termino del intervalo, separados por una coma (x_{0}, x_{n})")
+user_yf = readline()
+yf = split(user_yf, ',')
+yf= parse.(Float64, yf)
+println(yf)
 
-
-println("Ingrese  (U(xo), U(xn)) separados por una coma ")
-user_uxf = readline()
-uxf = split(user_uxf, ',')
-uxf= parse.(Float64, uxf)
-println(uxf)
+println("Ingrese (U_{o}, U_{1}, U_{n-1}, U_{n-2}) separados por una coma ")
+user_uyf = readline()
+uyf = split(user_uyf, ',')
+uyf= parse.(Float64, uyf)
+println(uyf)
 
 println("Ingrese la cantidad de puntos en dividir el intervalo (n)")
 n = readline()
@@ -110,9 +126,12 @@ println("Ingrese el valor de epsilon")
 ep = readline()
 ep = parse(Float64, ep)
 
-h = (xf[2]-xf[1])/n
+h = (yf[2]-yf[1])/n 
+
+y1= yf[1]+h
 
 A= matrixBuilder()
+
 b = rvecBuilder()
 println(b)
 
@@ -123,3 +142,11 @@ max_iteraciones = 1000000
 
 solucion = gauss_seidel(A, b, x0, tolerancia, max_iteraciones)
 println("Solución: ", solucion)
+
+xs= range(yf[1], yf[2], step=h)
+println(length(xs))
+
+system = vcat(uyf[1:2],solucion,uyf[3:4])
+println(system)
+println(length(system))
+plot(xs, system)
